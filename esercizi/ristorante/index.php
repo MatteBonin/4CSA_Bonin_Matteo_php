12fe6cd9fedb1737103c797d758e0535b0a29d91
MatteBonin <146830844+MatteBonin@users.noreply.github.com>
2023-11-16 11:58:26 +0000

    Modifica inutile paragrafo

--- esercizi/ristorante/index.php
+++ esercizi/ristorante/index.php
@@ -20,7 +20,7 @@
   </header>
   <main class="px-3">
     <h1>Menù</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <p class="lead">Dal "Gambero Rosso" potete trovare nu</p>
     <p class="lead">
       <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
     </p>
