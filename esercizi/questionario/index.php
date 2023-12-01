c9eec5639868295514100391905d71d6ef82fb61
MatteBonin <146830844+MatteBonin@users.noreply.github.com>
2023-12-01 21:46:18 +0000

    Creazione php

--- esercizi/questionario/index.php
+++ esercizi/questionario/index.php
@@ -5,31 +5,41 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
     <body>
-    <form class="signIn">
+    <form class="signIn" method="get">
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <label for="exampleInputPassword1" class="form-label"><h2>Password</h2></label>
+            <input type="password" class="form-control" id="Password" placeholder="Password" name="Password">
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-warning">Invia Password</button>
     </form>
+    <?php 
+        if($_GET["Password"]=="info2023")
+        {
+    ?>
+        <div class="d-flex justify-content-center">
+                <div class="spinner-border text-warning" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    <?php
+        }
+    ?>
     </body>
 </html>
 
 <style>
-    .body{
-        width: 100%;
+    body{
+        background-color: rgb(248, 249, 250);
     }
     .signIn{
-        max-width: 330px;
-        padding: 1rem ;
+        width: 400px;
+        padding: 2rem;
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        border: 5px solid #ffc107;
+        border-radius: 20px;
     }
 </style>
